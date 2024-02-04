e71579af72e51b044a6a2f2cab54b5ec88881f3f
PashaBudzin <pashabudzin@gmail.com>
2024-02-04 17:59:19 +0200

    final version of config

--- lua/pasha/config/lsp.lua
+++ lua/pasha/config/lsp.lua
@@ -1,31 +1,30 @@
 local lsp = require("lsp-zero")
 
 require('mason').setup({
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗"
-		}
-	}
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
 })
-
 require('mason-lspconfig').setup({
-	ensure_installed = { "lua_ls", "rust_analyzer", "jsonls", "yamlls" },
-	handlers = {
-		lsp.default_setup,
-		rust_analyzer = function()
-			local rust_tools = require('rust-tools')
+    ensure_installed = { "lua_ls", "rust_analyzer", "jsonls", "yamlls" },
+    handlers = {
+        lsp.default_setup,
+        rust_analyzer = function()
+            local rust_tools = require('rust-tools')
 
-			rust_tools.setup({
-				server = {
-					on_attach = function(_, bufnr)
-						vim.keymap.set('n', '<leader>ca', rust_tools.hover_actions.hover_actions, { buffer = bufnr })
-					end
-				}
-			})
-		end
-	},
+            rust_tools.setup({
+                server = {
+                    on_attach = function(_, bufnr)
+                        vim.keymap.set('n', '<leader>ca', rust_tools.hover_actions.hover_actions, { buffer = bufnr })
+                    end
+                }
+            })
+        end
+    },
 })
 
 
@@ -46,50 +45,50 @@ local cmp_action = require('lsp-zero').cmp_action()
 
 
 cmp.setup({
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-f>'] = cmp_action.luasnip_jump_forward(),
-		['<C-b>'] = cmp_action.luasnip_jump_backward(),
-		['<C-u>'] = cmp.mapping.scroll_docs(-4),
-		['<C-d>'] = cmp.mapping.scroll_docs(4),
-		['<tab>'] = cmp.mapping.confirm({ select = true }),
-	})
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<tab>'] = cmp.mapping.confirm({ select = true }),
+    })
 })
 
 
 lsp.set_preferences({
-	suggest_lsp_servers = false,
-	sign_icons = {
-		error = 'E',
-		warn = 'W',
-		hint = 'H',
-		info = 'I'
-	}
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
 })
 
 
 lsp.on_attach(function(client, bufnr)
-	local opts = { buffer = bufnr, remap = false }
+    local opts = { buffer = bufnr, remap = false }
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-	vim.notify(string.format("Loaded %s", client.name))
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.notify(string.format("Loaded %s", client.name))
 
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
-	end, { desc = 'Format current buffer with LSP' })
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
 end)
 
 require('luasnip.loaders.from_vscode').lazy_load()
@@ -98,6 +97,6 @@ require('luasnip.loaders.from_vscode').lazy_load()
 lsp.setup()
 
 vim.diagnostic.config({
-	virtual_text = true,
-	severity_sort = true
+    virtual_text = true,
+    severity_sort = true
 })
--- lua/pasha/remap/lsp.lua
+++ lua/pasha/remap/lsp.lua
@@ -1,27 +1,26 @@
 local wk = require("which-key")
 
 wk.register({
-	["<leader>"] = {
-		e = "open diagnostics",
-		v = {
-			name = "rust tools",
-			c = {
-				name = "actions",
-				a = "code actions"
-			},
-			r = {
-				name = "references",
-				n = "rename",
-				r = "find references"
-			},
-			w = {
-				name = "query",
-					
-			}
-		}
-	},
-	["g"] = {
-		d = "[G]o to [D]efinitions"
-	}
-})
+    ["<leader>"] = {
+        e = "open diagnostics",
+        v = {
+            name = "lsp actions",
+            c = {
+                name = "actions",
+                a = "code actions"
+            },
+            r = {
+                name = "references",
+                n = "rename",
+                r = "find references"
+            },
+            w = {
+                name = "query",
 
+            }
+        },
+    },
+    ["g"] = {
+        d = "[G]o to [D]efinitions"
+    },
+})
