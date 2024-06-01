b4cd34f251150faa7270f403bad09a00056a3d35
= <pashabudzin@proton.me>
2024-06-01 15:43:38 +0300

    add vim wiki

---
+++ lua/pasha/plugins/wiki.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "vimwiki/vimwiki",
+        init = function()
+            vim.g.vimwiki_list = {
+                {
+                    path = '~/Documents/Notes',
+                    syntax = 'markdown',
+                    ext = '.md',
+                },
+            }
+        end
+    }
+}
